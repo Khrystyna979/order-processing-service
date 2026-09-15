60f249d41a28b54d76a291e7c76dd1e7e6b34df4
Khrystyna979 <khrystynaa.ol@gmail.com>
2026-09-15 18:49:40 +0300

    fix(products): raise 404 HTTPException when product is not found

--- src/products/routes.py
+++ src/products/routes.py
@@ -15,9 +15,11 @@ async def read_products(skip: int = 0, limit: int = 10, db: AsyncSession = Depen
     return products
     
 @router.get('/{product_id}', response_model=ProductResponse)
-async def read_products(product_id: uuid.UUID, db: AsyncSession = Depends(get_db)):
+async def read_product(product_id: uuid.UUID, db: AsyncSession = Depends(get_db)):
     product = await repository_products.read_product(product_id, db)
-    return product
+    if product:
+        return product
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Product not found')
     
 @router.post('/', response_model=ProductResponse, status_code=status.HTTP_201_CREATED)
 async def create_product(body: ProductCreate, db: AsyncSession = Depends(get_db)):
