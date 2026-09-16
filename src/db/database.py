4e4c17304eb0f197a4af58877216cb2e7831112a
Khrystyna979 <khrystynaa.ol@gmail.com>
2026-09-16 13:07:42 +0300

    fix(db): set expire_on_commit to False in sessionmaker

--- src/db/database.py
+++ src/db/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import DeclarativeBase
 
 engine = create_async_engine(settings.DATABASE_URL, echo=True)
 
-SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
+SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 class Base(DeclarativeBase):
     ...
